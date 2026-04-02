0679b2987f0799cdcf55f15d8f6bdfb3925cd1ee
whelan <brickhousedevelopers@gmail.com>
2026-04-02 13:18:33 -0400

    Rstudio install

--- files/scripts/install-rstudio.sh
+++ files/scripts/install-rstudio.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -ouex pipefail
 
+# Import Posit's GPG signing key
+rpm --import https://cdn.posit.co/signing-key/posit-signing-key-public.asc
+
 # Download RStudio daily build directly from Posit's S3 bucket
 curl -fL -o /tmp/rstudio.rpm \
   "https://s3.amazonaws.com/rstudio-ide-build/electron/rhel9/x86_64/rstudio-2026.04.0-daily-465-x86_64.rpm"
