036f4f317f5aba938bb8747dc7e508aeda6405a2
whelan <brickhousedevelopers@gmail.com>
2026-04-13 10:46:36 -0400

    revise remove openh264 script

--- files/scripts/disable-openh264-repo.sh
+++ files/scripts/disable-openh264-repo.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Disable fedora-cisco-openh264 repository
-rpm-ostree ex setopt fedora-cisco-openh264.enabled=0
+# Disable fedora-cisco-openh264 repository by removing its repo file
+if [ -f /etc/yum.repos.d/fedora-cisco-openh264.repo ]; then
+    rm -f /etc/yum.repos.d/fedora-cisco-openh264.repo
+    echo "Removed fedora-cisco-openh264.repo"
+fi
+
+# Alternative: disable it in place if removal doesn't work
+if [ -f /etc/yum.repos.d/fedora-cisco-openh264.repo ]; then
+    sed -i 's/enabled=1/enabled=0/g' /etc/yum.repos.d/fedora-cisco-openh264.repo
+    echo "Disabled fedora-cisco-openh264 repository"
+fi
